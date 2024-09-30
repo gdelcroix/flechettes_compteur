7a97c6d46358c16b2188b391f285c83cf4014794
gdelcroix <4097422+gdelcroix@users.noreply.github.com>
2024-09-30 12:40:04 +0200

    On branch master Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Assets/dart.png 	new file:   src/Components/scoreIndicator.jsx 	new file:   src/Components/styles.css

---
+++ src/Components/scoreIndicator.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactScoreIndicator from "react-score-indicator";
+
+import "./styles.css";
+
+const scoreIndicator = ({value, maxValue}) => {
+    return (
+      <div className="indicator w-auto mt-9">
+        <span className="w-auto">
+          <ReactScoreIndicator
+            value={value}
+            maxValue={maxValue}
+            maxAngle={260}
+            width={300}            
+            lineWidth={29}
+          />
+        </span>
+      </div>
+    );
+};
+export default scoreIndicator;
